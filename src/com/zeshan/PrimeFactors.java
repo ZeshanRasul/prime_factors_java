b6ad7e1558f7ce64f99f361a99ddb90164e8ac3c
ZeshanRasul <zeshanrasul90@gmail.com>
2016-12-24 21:15:45 +0000

    Project set up and gitignore

---
+++ src/com/zeshan/PrimeFactors.java
@@ -0,0 +1,9 @@
+package com.zeshan;
+
+public class PrimeFactors {
+
+    public static void main(String[] args) {
+    }
+
+
+}
---
+++ test/com/zeshan/PrimeFactorsTest.java
@@ -0,0 +1,10 @@
+package com.zeshan;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zeshanrasul on 24/12/2016.
+ */
+public class PrimeFactorsTest {
+
+}
