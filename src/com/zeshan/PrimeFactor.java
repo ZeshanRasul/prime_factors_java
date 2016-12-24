5e170523048f6f3b8757e4cc1ec2445b203bb717
ZeshanRasul <zeshanrasul90@gmail.com>
2016-12-24 22:01:30 +0000

    Prime Factor generate() method passes test, returns correct values

--- src/com/zeshan/PrimeFactor.java
+++ src/com/zeshan/PrimeFactor.java
@@ -1,9 +1,25 @@
 package com.zeshan;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactor {
 
     public static void main(String[] args) {
     }
 
 
+    public List<Integer> generate(int n) {
+        int i = 2;
+        List<Integer> generatedFactors = new ArrayList<Integer>();
+        while (n > 1) {
+            if (n % i == 0) {
+                generatedFactors.add(i);
+                n = n/i;
+            } else {
+                i++;
+            }
+        }
+        return generatedFactors;
+    }
 }
